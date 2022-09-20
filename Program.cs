872290a11560934e935f9d43a65ac2d2cfb46b2b
francescoP02 <file.partipilo@gmail.com>
2022-09-20 14:26:52 +0200

    Added User

--- Program.cs
+++ Program.cs
@@ -7,7 +7,8 @@
 //email,
 //password,
 //recapito telefonico,
-//Gli utenti registrati possono effettuare dei prestiti sui documenti che sono di vario tipo (libri, DVD). I documenti sono caratterizzati da:
+//Gli utenti registrati possono effettuare dei prestiti sui documenti che sono di vario tipo (libri, DVD).
+//I documenti sono caratterizzati da:
 //un codice identificativo di tipo stringa (ISBN per i libri, numero seriale per i DVD),
 //titolo,
 //anno,
@@ -16,5 +17,20 @@
 //uno scaffale in cui è posizionato,
 //un autore (Nome, Cognome).
 //Per i libri si ha in aggiunta il numero di pagine, mentre per i dvd la durata.
-//L’utente deve poter eseguire delle ricerche per codice o per titolo e, eventualmente, effettuare dei prestiti registrando il periodo (Dal/Al) del prestito e il documento.
+//L’utente deve poter eseguire delle ricerche per codice o per titolo e, eventualmente,
+//effettuare dei prestiti registrando il periodo (Dal/Al) del prestito e il documento.
 //Deve essere possibile effettuare la ricerca dei prestiti dato nome e cognome di un utente.
+
+List<User> registeredUser = new List<User>();
+
+//utenti registrati
+registeredUser.Add(new User("Prova", "Rossi", "rossi@gmail.com", "12345a", "3331112233", true));
+registeredUser.Add(new User("Prova", "Gialli", "gialli@gmail.com", "12345b", "3332221133", true));
+registeredUser.Add(new User("Prova", "Verdi", "verdi@gmail.com", "12345c", "3333332211", true));
+
+foreach (var user in registeredUser)
+{
+    Console.WriteLine(user.name);
+    Console.WriteLine(user.surname);
+}
+
